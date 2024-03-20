f473bf41722e1f43833a3938116eb4dbef90f4f0
DESKTOP-CKJJILT\admin <pateldhruv123890@gmail.com>
2024-03-20 23:10:08 +0530

    swagger fix

--- src/common/config/swagger.config.js
+++ src/common/config/swagger.config.js
@@ -1,34 +1,35 @@
 import express from "express";
 import { serve, setup } from "swagger-ui-express";
 import { baseUrl } from "./constant.config";
+const path = require("path");
 
 const YAML = require("yamljs");
 
 const router = express.Router();
 console.log("in");
-const swaggerDocument = YAML.load("swagger.yaml");
+const swaggerDocument = YAML.load(
+  path.join(__dirname, "../../../swagger.yaml")
+);
 console.log("in2", swaggerDocument);
 
-if (process.env.ENV !== "production") {
-  router.use(
-    "/api/documentation",
-    (req, res, next) => {
-      swaggerDocument.info.title = process.env.APP_NAME;
-      swaggerDocument.servers = [
-        {
-          url: baseUrl() + "/api/v1",
-          description: "API base url",
-        },
-      ];
-      req.swaggerDoc = swaggerDocument;
-      next();
-    },
-    serve,
-    setup(swaggerDocument, {
-      swaggerOptions: {
-        persistAuthorization: true,
+router.use(
+  "/api/documentation",
+  (req, res, next) => {
+    swaggerDocument.info.title = process.env.APP_NAME;
+    swaggerDocument.servers = [
+      {
+        url: baseUrl() + "/api/v1",
+        description: "API base url",
       },
-    })
-  );
-}
+    ];
+    req.swaggerDoc = swaggerDocument;
+    next();
+  },
+  serve,
+  setup(swaggerDocument, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  })
+);
 export default router;
